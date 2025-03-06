8f18ea29003be23e183e49dd2a98e39d7c23b4ea
Georgiy <sadkovdanil64@gmail.com>
2025-03-06 23:33:20 +0300

    feat: created a theme constant, removed the theme typing that I added earlier, left it simply as const

--- src/shared/const/theme/index.const.ts
+++ src/shared/const/theme/index.const.ts
@@ -1,6 +1,10 @@
+
 export const theme = {
 	colors: {
 		background: '#06080c',
-		color: "#FFFFFF"
-	}
-} as const 
+		color: "#FFFFFF",
+		buttonBackground: '#EB0237',
+		buttonColor: '#FFFFFF',
+		buttonBackgroundHover: '#A01131'
+	} 
+} as const
--- src/shared/types/theme/index.type.ts
+++
@@ -1,6 +0,0 @@
-export type Theme = {
-	colors: {
-		background: string // Цвет фона
-		color: string // Цвет текста
-	}
-}
