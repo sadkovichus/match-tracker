8db6aaa581f8573a4d62c831c933430738dabb81
Georgiy <sadkovdanil64@gmail.com>
2025-03-09 20:19:12 +0300

    chore: update style theme

--- src/shared/const/theme/index.const.ts
+++ src/shared/const/theme/index.const.ts
@@ -3,8 +3,29 @@ export const theme = {
 	colors: {
 		background: '#06080c',
 		color: "#FFFFFF",
+		secondaryColor: '#6e6f72',
+
+		errorBackground: '#0F1318',
+
+		// button updating
+		buttonBackgroundUpdating: '#701328',
+		buttonColorUpdating: '#787878',
+
+		// button static
 		buttonBackground: '#EB0237',
 		buttonColor: '#FFFFFF',
-		buttonBackgroundHover: '#A01131'
-	} 
+		buttonBackgroundHover: '#A01131',
+
+		cardBackground: '#0B0E12',
+
+		statBlockColor: '#101318'
+	},
+	statusColors: {
+		Ongoing: '#43AD28',
+		Finished: '#EB0237',
+		Scheduled: '#EB6402',
+	},
+	blocks: {
+		borderRadius: 'border-radius: 4px',
+	}
 } as const
